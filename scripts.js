70dc77a2c0f285f6f8b7a87ce595b9d9a6b03d2e
tejavoo <teja.3b@gmail.com>
2016-04-24 23:19:23 +0530

    iitm5 to iitm

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var app = angular.module('putApp', ['ngMaterial']);
     app.controller('putCtrl',['$scope','$http',function($scope,$http){
-      $http.get("http://iitm5.cloudapp.net/api/get_items_today/").then(function(response) {
+      $http.get("http://iitm.cloudapp.net/api/get_items_today/").then(function(response) {
         $scope.putData = response.data;
       });
       
